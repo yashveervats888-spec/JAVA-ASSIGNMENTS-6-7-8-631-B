2cdadb913e6472e80348f6d7e48d52f282cdc16a
yashveervats888-spec <yashveervats888@gmail.com>
2025-11-04 15:45:09 +0530

    Create 6.3-2 23BCS10361.java

---
+++ 6.3-2 23BCS10361.java
@@ -0,0 +1,20 @@
+package com.mvc;
+
+public class Student {
+    private int studentID;
+    private String name;
+    private String department;
+    private double marks;
+
+    public Student(int studentID, String name, String department, double marks) {
+        this.studentID = studentID;
+        this.name = name;
+        this.department = department;
+        this.marks = marks;
+    }
+
+    public int getStudentID() { return studentID; }
+    public String getName() { return name; }
+    public String getDepartment() { return department; }
+    public double getMarks() { return marks; }
+}
